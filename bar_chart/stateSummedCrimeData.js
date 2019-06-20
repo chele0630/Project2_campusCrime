e3b5ea823a8e0c93bc19aa08de205a77d079c307
Mabel G <47480625+mabel912@users.noreply.github.com>
2019-06-19 20:08:50 -0500

    Add files via upload

---
+++ bar_chart/stateSummedCrimeData.js
@@ -0,0 +1,356 @@
+var data =[
+  {
+    "State": "AK",
+    "Total15": 38,
+    "Total16": 52,
+    "Total17": 42
+  },
+  {
+    "State": "AL",
+    "Total15": 474,
+    "Total16": 465,
+    "Total17": 481
+  },
+  {
+    "State": "AR",
+    "Total15": 279,
+    "Total16": 279,
+    "Total17": 307
+  },
+  {
+    "State": "AS",
+    "Total15": 12,
+    "Total16": 2,
+    "Total17": 0
+  },
+  {
+    "State": "AZ",
+    "Total15": 356,
+    "Total16": 400,
+    "Total17": 443
+  },
+  {
+    "State": "CA",
+    "Total15": 3287,
+    "Total16": 3419,
+    "Total17": 3495
+  },
+  {
+    "State": "CO",
+    "Total15": 439,
+    "Total16": 518,
+    "Total17": 513
+  },
+  {
+    "State": "CT",
+    "Total15": 519,
+    "Total16": 466,
+    "Total17": 389
+  },
+  {
+    "State": "DC",
+    "Total15": 323,
+    "Total16": 394,
+    "Total17": 291
+  },
+  {
+    "State": "DE",
+    "Total15": 107,
+    "Total16": 75,
+    "Total17": 103
+  },
+  {
+    "State": "FL",
+    "Total15": 961,
+    "Total16": 989,
+    "Total17": 994
+  },
+  {
+    "State": "FM",
+    "Total15": 10,
+    "Total16": 5,
+    "Total17": 11
+  },
+  {
+    "State": "GA",
+    "Total15": 733,
+    "Total16": 754,
+    "Total17": 763
+  },
+  {
+    "State": "GU",
+    "Total15": 0,
+    "Total16": 16,
+    "Total17": 13
+  },
+  {
+    "State": "HI",
+    "Total15": 132,
+    "Total16": 123,
+    "Total17": 136
+  },
+  {
+    "State": "IA",
+    "Total15": 556,
+    "Total16": 532,
+    "Total17": 508
+  },
+  {
+    "State": "ID",
+    "Total15": 145,
+    "Total16": 126,
+    "Total17": 124
+  },
+  {
+    "State": "IL",
+    "Total15": 841,
+    "Total16": 981,
+    "Total17": 1034
+  },
+  {
+    "State": "IN",
+    "Total15": 927,
+    "Total16": 823,
+    "Total17": 840
+  },
+  {
+    "State": "KS",
+    "Total15": 306,
+    "Total16": 273,
+    "Total17": 294
+  },
+  {
+    "State": "KY",
+    "Total15": 355,
+    "Total16": 434,
+    "Total17": 323
+  },
+  {
+    "State": "LA",
+    "Total15": 518,
+    "Total16": 514,
+    "Total17": 554
+  },
+  {
+    "State": "MA",
+    "Total15": 1114,
+    "Total16": 1140,
+    "Total17": 1102
+  },
+  {
+    "State": "MD",
+    "Total15": 479,
+    "Total16": 433,
+    "Total17": 469
+  },
+  {
+    "State": "ME",
+    "Total15": 171,
+    "Total16": 193,
+    "Total17": 138
+  },
+  {
+    "State": "MH",
+    "Total15": 1,
+    "Total16": 1,
+    "Total17": 6
+  },
+  {
+    "State": "MI",
+    "Total15": 865,
+    "Total16": 822,
+    "Total17": 885
+  },
+  {
+    "State": "MN",
+    "Total15": 415,
+    "Total16": 401,
+    "Total17": 544
+  },
+  {
+    "State": "MO",
+    "Total15": 564,
+    "Total16": 621,
+    "Total17": 711
+  },
+  {
+    "State": "MP",
+    "Total15": 0,
+    "Total16": 0,
+    "Total17": 0
+  },
+  {
+    "State": "MS",
+    "Total15": 369,
+    "Total16": 317,
+    "Total17": 289
+  },
+  {
+    "State": "MT",
+    "Total15": 81,
+    "Total16": 89,
+    "Total17": 61
+  },
+  {
+    "State": "NC",
+    "Total15": 1068,
+    "Total16": 1170,
+    "Total17": 1273
+  },
+  {
+    "State": "ND",
+    "Total15": 67,
+    "Total16": 56,
+    "Total17": 94
+  },
+  {
+    "State": "NE",
+    "Total15": 180,
+    "Total16": 170,
+    "Total17": 367
+  },
+  {
+    "State": "NH",
+    "Total15": 189,
+    "Total16": 194,
+    "Total17": 227
+  },
+  {
+    "State": "NJ",
+    "Total15": 421,
+    "Total16": 532,
+    "Total17": 495
+  },
+  {
+    "State": "NM",
+    "Total15": 487,
+    "Total16": 496,
+    "Total17": 487
+  },
+  {
+    "State": "NV",
+    "Total15": 90,
+    "Total16": 108,
+    "Total17": 145
+  },
+  {
+    "State": "NY",
+    "Total15": 1696,
+    "Total16": 1684,
+    "Total17": 1874
+  },
+  {
+    "State": "OH",
+    "Total15": 939,
+    "Total16": 1250,
+    "Total17": 1189
+  },
+  {
+    "State": "OK",
+    "Total15": 362,
+    "Total16": 410,
+    "Total17": 367
+  },
+  {
+    "State": "OR",
+    "Total15": 555,
+    "Total16": 550,
+    "Total17": 505
+  },
+  {
+    "State": "PA",
+    "Total15": 1292,
+    "Total16": 1223,
+    "Total17": 1263
+  },
+  {
+    "State": "PR",
+    "Total15": 142,
+    "Total16": 131,
+    "Total17": 109
+  },
+  {
+    "State": "PW",
+    "Total15": 0,
+    "Total16": 0,
+    "Total17": 0
+  },
+  {
+    "State": "RI",
+    "Total15": 182,
+    "Total16": 207,
+    "Total17": 176
+  },
+  {
+    "State": "SC",
+    "Total15": 506,
+    "Total16": 484,
+    "Total17": 496
+  },
+  {
+    "State": "SD",
+    "Total15": 60,
+    "Total16": 82,
+    "Total17": 55
+  },
+  {
+    "State": "TN",
+    "Total15": 604,
+    "Total16": 583,
+    "Total17": 593
+  },
+  {
+    "State": "TX",
+    "Total15": 1618,
+    "Total16": 1741,
+    "Total17": 1736
+  },
+  {
+    "State": "UT",
+    "Total15": 228,
+    "Total16": 242,
+    "Total17": 237
+  },
+  {
+    "State": "VA",
+    "Total15": 727,
+    "Total16": 785,
+    "Total17": 763
+  },
+  {
+    "State": "VI",
+    "Total15": 4,
+    "Total16": 8,
+    "Total17": 6
+  },
+  {
+    "State": "VT",
+    "Total15": 185,
+    "Total16": 152,
+    "Total17": 161
+  },
+  {
+    "State": "WA",
+    "Total15": 518,
+    "Total16": 545,
+    "Total17": 547
+  },
+  {
+    "State": "WI",
+    "Total15": 485,
+    "Total16": 411,
+    "Total17": 454
+  },
+  {
+    "State": "WV",
+    "Total15": 209,
+    "Total16": 180,
+    "Total17": 169
+  },
+  {
+    "State": "WY",
+    "Total15": 62,
+    "Total16": 43,
+    "Total17": 54
+  }
+];
