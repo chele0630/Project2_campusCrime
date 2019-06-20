01d99190f9f84e67f03d155cc4fa92132267cede
Massudi09 <47423178+Massudi09@users.noreply.github.com>
2019-06-20 14:34:46 -0500

    Add files via upload

---
+++ data/data_merged.js
@@ -0,0 +1,603 @@
+var mergedData =  
+  [
+        {
+        "location": [
+            32.7794,
+            -86.8287
+        ],
+        "name": "Alabama",
+        "rating": "4 stars",
+        "State": "Alabama",
+        "Total15": 474,
+        "Total16": 465,
+        "Total17": 481
+        },
+        {
+        "location": [
+            64.0685,
+            -152.2782
+        ],
+        "name": "Alaska",
+        "rating": "5 stars",
+        "State": "Alaska",
+        "Total15": 38,
+        "Total16": 52,
+        "Total17": 42
+        },
+        {
+        "location": [
+            34.8938,
+            -92.4426
+        ],
+        "name": "Arkansas",
+        "rating": "3 stars",
+        "State": "Arkansas",
+        "Total15": 279,
+        "Total16": 279,
+        "Total17": 307
+        },
+        {
+        "location": [
+            34.2744,
+            -111.6602
+        ],
+        "name": "Arizona",
+        "rating": "5 stars",
+        "State": "Arizona",
+        "Total15": 356,
+        "Total16": 400,
+        "Total17": 443
+        },
+        {
+        "location": [
+            37.1841,
+            -119.4696
+        ],
+        "name": "California",
+        "rating": "1 star",
+        "State": "California",
+        "Total15": 3287,
+        "Total16": 3419,
+        "Total17": 3495
+        },
+        {
+        "location": [
+            38.9972,
+            -105.5478
+        ],
+        "name": "Colorado",
+        "rating": "3 stars",
+        "State": "Colorado",
+        "Total15": 439,
+        "Total16": 518,
+        "Total17": 513
+        },
+        {
+        "location": [
+            41.6219,
+            -72.7273
+        ],
+        "name": "Connecticut",
+        "rating": "3 stars",
+        "State": "Connecticut",
+        "Total15": 519,
+        "Total16": 466,
+        "Total17": 389
+        },
+        {
+        "location": [
+            38.9896,
+            -75.505
+        ],
+        "name": "Delaware",
+        "rating": "2 stars",
+        "State": "Delaware",
+        "Total15": 107,
+        "Total16": 75,
+        "Total17": 103
+        },
+        {
+        "location": [
+            28.6305,
+            -82.4497
+        ],
+        "name": "Florida",
+        "rating": "4 stars",
+        "State": "Florida",
+        "Total15": 961,
+        "Total16": 989,
+        "Total17": 994
+        },
+        {
+        "location": [
+            32.6415,
+            -83.4426
+        ],
+        "name": "Georgia",
+        "rating": "4 stars",
+        "State": "Georgia",
+        "Total15": 733,
+        "Total16": 754,
+        "Total17": 763
+        },
+        {
+        "location": [
+            20.2927,
+            -156.3737
+        ],
+        "name": "Hawaii",
+        "rating": "1 star",
+        "State": "Hawaii",
+        "Total15": 132,
+        "Total16": 123,
+        "Total17": 136
+        },
+        {
+        "location": [
+            44.3509,
+            -114.613
+        ],
+        "name": "Idaho",
+        "rating": "5 stars",
+        "State": "Idaho",
+        "Total15": 145,
+        "Total16": 126,
+        "Total17": 124
+        },
+        {
+        "location": [
+            40.0417,
+            -89.1965
+        ],
+        "name": "Illinois",
+        "rating": "2 stars",
+        "State": "Illinois",
+        "Total15": 841,
+        "Total16": 981,
+        "Total17": 1034
+        },
+        {
+        "location": [
+            39.8942,
+            -86.2816
+        ],
+        "name": "Indiana",
+        "rating": "4 stars",
+        "State": "Indiana",
+        "Total15": 927,
+        "Total16": 823,
+        "Total17": 840
+        },
+        {
+        "location": [
+            42.0751,
+            -93.496
+        ],
+        "name": "Iowa",
+        "rating": "3 stars",
+        "State": "Iowa",
+        "Total15": 556,
+        "Total16": 532,
+        "Total17": 508
+        },
+        {
+        "location": [
+            38.4937,
+            -98.3804
+        ],
+        "name": "Kansas",
+        "rating": "5 stars",
+        "State": "Kansas",
+        "Total15": 306,
+        "Total16": 273,
+        "Total17": 294
+        },
+        {
+        "location": [
+            37.5347,
+            -85.3021
+        ],
+        "name": "Kentucky",
+        "rating": "4 stars",
+        "State": "Kentucky",
+        "Total15": 355,
+        "Total16": 434,
+        "Total17": 323
+        },
+        {
+        "location": [
+            31.0689,
+            -91.9968
+        ],
+        "name": "Louisiana",
+        "rating": "3 stars",
+        "State": "Louisiana",
+        "Total15": 518,
+        "Total16": 514,
+        "Total17": 554
+        },
+        {
+        "location": [
+            45.3695,
+            -69.2428
+        ],
+        "name": "Maine",
+        "rating": "5 stars",
+        "State": "Maine",
+        "Total15": 171,
+        "Total16": 193,
+        "Total17": 138
+        },
+        {
+        "location": [
+            39.055,
+            -76.7909
+        ],
+        "name": "Maryland",
+        "rating": "1 star",
+        "State": "Maryland",
+        "Total15": 479,
+        "Total16": 433,
+        "Total17": 469
+        },
+        {
+        "location": [
+            42.2596,
+            -71.8083
+        ],
+        "name": "Massachusetts",
+        "rating": "2 stars",
+        "State": "Massachusetts",
+        "Total15": 1114,
+        "Total16": 1140,
+        "Total17": 1102
+        },
+        {
+        "location": [
+            44.3467,
+            -85.4102
+        ],
+        "name": "Michigan",
+        "rating": "3 stars",
+        "State": "Michigan",
+        "Total15": 865,
+        "Total16": 822,
+        "Total17": 885
+        },
+        {
+        "location": [
+            46.2807,
+            -94.3053
+        ],
+        "name": "Minnesota",
+        "rating": "2 stars",
+        "State": "Minnesota",
+        "Total15": 415,
+        "Total16": 401,
+        "Total17": 544
+        },
+        {
+        "location": [
+            32.7364,
+            -89.6678
+        ],
+        "name": "Mississippi",
+        "rating": "5 stars",
+        "State": "Mississippi",
+        "Total15": 369,
+        "Total16": 317,
+        "Total17": 289
+        },
+        {
+        "location": [
+            38.3566,
+            -92.458
+        ],
+        "name": "Missouri",
+        "rating": "5 stars",
+        "State": "Missouri",
+        "Total15": 564,
+        "Total16": 621,
+        "Total17": 711
+        },
+        {
+        "location": [
+            47.0527,
+            -109.6333
+        ],
+        "name": "Montana",
+        "rating": "4 stars",
+        "State": "Montana",
+        "Total15": 81,
+        "Total16": 89,
+        "Total17": 61
+        },
+        {
+        "location": [
+            41.5378,
+            -99.7951
+        ],
+        "name": "Nebraska",
+        "rating": "3 stars",
+        "State": "Nebraska",
+        "Total15": 180,
+        "Total16": 170,
+        "Total17": 367
+        },
+        {
+        "location": [
+            39.3289,
+            -116.6312
+        ],
+        "name": "Nevada",
+        "rating": "3 stars",
+        "State": "Nevada",
+        "Total15": 90,
+        "Total16": 108,
+        "Total17": 145
+        },
+        {
+        "location": [
+            43.6805,
+            -71.5811
+        ],
+        "name": "New Hampshire",
+        "rating": "5 stars",
+        "State": "New Hampshire",
+        "Total15": 189,
+        "Total16": 194,
+        "Total17": 227
+        },
+        {
+        "location": [
+            40.1907,
+            -74.6728
+        ],
+        "name": "New Jersey",
+        "rating": "1 star",
+        "State": "New Jersey",
+        "Total15": 421,
+        "Total16": 532,
+        "Total17": 495
+        },
+        {
+        "location": [
+            34.4071,
+            -106.1126
+        ],
+        "name": "New Mexico",
+        "rating": "4 stars",
+        "State": "New Mexico",
+        "Total15": 487,
+        "Total16": 496,
+        "Total17": 487
+        },
+        {
+        "location": [
+            42.9538,
+            -75.5268
+        ],
+        "name": "New York",
+        "rating": "1 star",
+        "State": "New York",
+        "Total15": 1696,
+        "Total16": 1684,
+        "Total17": 1874
+        },
+        {
+        "location": [
+            35.5557,
+            -79.3877
+        ],
+        "name": "North Carolina",
+        "rating": "4 stars",
+        "State": "North Carolina",
+        "Total15": 1068,
+        "Total16": 1170,
+        "Total17": 1273
+        },
+        {
+        "location": [
+            47.4501,
+            -100.4659
+        ],
+        "name": "North Dakota",
+        "rating": "4 stars",
+        "State": "North Dakota",
+        "Total15": 67,
+        "Total16": 56,
+        "Total17": 94
+        },
+        {
+        "location": [
+            40.2862,
+            -82.7937
+        ],
+        "name": "Ohio",
+        "rating": "4 stars",
+        "State": "Ohio",
+        "Total15": 939,
+        "Total16": 1250,
+        "Total17": 1189
+        },
+        {
+        "location": [
+            35.5889,
+            -97.4943
+        ],
+        "name": "Oklahoma",
+        "rating": "4 stars",
+        "State": "Oklahoma",
+        "Total15": 362,
+        "Total16": 410,
+        "Total17": 367
+        },
+        {
+        "location": [
+            43.9336,
+            -120.5583
+        ],
+        "name": "Oregon",
+        "rating": "3 stars",
+        "State": "Oregon",
+        "Total15": 555,
+        "Total16": 550,
+        "Total17": 505
+        },
+        {
+        "location": [
+            40.8781,
+            -77.7996
+        ],
+        "name": "Pennsylvania",
+        "rating": "4 stars",
+        "State": "Pennsylvania",
+        "Total15": 1292,
+        "Total16": 1223,
+        "Total17": 1263
+        },
+        {
+        "location": [
+            41.6762,
+            -71.5562
+        ],
+        "name": "Rhode Island",
+        "rating": "3 stars",
+        "State": "Rhode Island",
+        "Total15": 182,
+        "Total16": 207,
+        "Total17": 176
+        },
+        {
+        "location": [
+            33.9169,
+            -80.8964
+        ],
+        "name": "South Carolina",
+        "rating": "3 stars",
+        "State": "South Carolina",
+        "Total15": 506,
+        "Total16": 484,
+        "Total17": 496
+        },
+        {
+        "location": [
+            44.4443,
+            -100.2263
+        ],
+        "name": "South Dakota",
+        "rating": "4 stars",
+        "State": "South Dakota",
+        "Total15": 60,
+        "Total16": 82,
+        "Total17": 55
+        },
+        {
+        "location": [
+            35.858,
+            -86.3505
+        ],
+        "name": "Tennessee",
+        "rating": "4 stars",
+        "State": "Tennesse",
+        "Total15": 604,
+        "Total16": 583,
+        "Total17": 593
+        },
+        {
+        "location": [
+            31.4757,
+            -99.3312
+        ],
+        "name": "Texas",
+        "rating": "4 stars",
+        "State": "Texas",
+        "Total15": 1618,
+        "Total16": 1741,
+        "Total17": 1736
+        },
+        {
+        "location": [
+            39.3055,
+            -111.6703
+        ],
+        "name": "Utah",
+        "rating": "4 stars",
+        "State": "Utah",
+        "Total15": 228,
+        "Total16": 242,
+        "Total17": 237
+        },
+        {
+        "location": [
+            44.0687,
+            -72.6658
+        ],
+        "name": "Vermont",
+        "rating": "5 stars",
+        "State": "Vermont",
+        "Total15": 185,
+        "Total16": 152,
+        "Total17": 161
+        },
+        {
+        "location": [
+            37.5215,
+            -78.8537
+        ],
+        "name": "Virginia",
+        "rating": "4 stars",
+        "State": "Virginia",
+        "Total15": 727,
+        "Total16": 785,
+        "Total17": 763
+        },
+        {
+        "location": [
+            47.3826,
+            -120.4472
+        ],
+        "name": "Washington",
+        "rating": "3 stars",
+        "State": "Washington",
+        "Total15": 518,
+        "Total16": 545,
+        "Total17": 547
+        },
+        {
+        "location": [
+            38.6409,
+            -80.6227
+        ],
+        "name": "West Virginia",
+        "rating": "5 stars",
+        "State": "West Virginia",
+        "Total15": 209,
+        "Total16": 180,
+        "Total17": 169
+        },
+        {
+        "location": [
+            44.6243,
+            -89.9941
+        ],
+        "name": "Wisconsin",
+        "rating": "4 stars",
+        "State": "Wisconsin",
+        "Total15": 485,
+        "Total16": 411,
+        "Total17": 454
+        },
+        {
+        "location": [
+            42.9957,
+            -107.5512
+        ],
+        "name": "Wyoming",
+        "rating": "5 stars",
+        "State": "Wyoming",
+        "Total15": 62,
+        "Total16": 43,
+        "Total17": 54
+        }
+    ];
