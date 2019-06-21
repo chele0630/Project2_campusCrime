afc1bd1ad60d29421d487fe57fa8e3d0676d0203
Massudi09 <47423178+Massudi09@users.noreply.github.com>
2019-06-20 23:45:34 -0500

    Update logic_heatmap.js

--- logic_heatmap.js
+++ logic_heatmap.js
@@ -227,10 +227,17 @@ maxZoom: 18,
 id: "mapbox.dark",
 accessToken: API_KEY
 })
+var street = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 18,
+id: "mapbox.streets",
+accessToken: API_KEY
+})
 // Only one base layer can be shown at a time
 var baseMaps = {
     Light: light,
-    Dark: dark
+    Dark: dark,
+    Streets: street
   };
   
 
